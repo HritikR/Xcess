c52444d68b6f96865ee73118272f9647e9de188c
Hritik R <35923605+HritikR@users.noreply.github.com>
2023-06-08 22:43:37 +0530

    Added post(), put(), delete(), all() handler

--- index.js
+++ index.js
@@ -20,6 +20,26 @@ class Xpress {
         this.routes.GET[path] = handler;
     }
 
+    // POST method handler
+    post(path, handler) {
+        this.routes.POST[path] = handler;
+    }
+
+    // PUT method handler
+    put(path, handler) {
+        this.routes.PUT[path] = handler;
+    }
+
+    // DELETE method handler
+    delete(path, handler) {
+        this.routes.DELETE[path] = handler;
+    }
+
+    // ALL method handler
+    all(path, handler) {
+        this.routes.ALL[path] = handler;
+    }
+
     // Start listening on the specified port with the given callback
     listen(port, callback) {
         const server = http.createServer();
